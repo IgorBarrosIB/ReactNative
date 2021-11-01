83f894824c9128a4774a6f8fd58407cd3b9ffe07
igor sousa <igor.sousa.terceirizado@icmbio.gov.br>
2021-10-31 22:00:52 -0300

    Treinando

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,123 +1,63 @@
 import React, {Fragment, useState, useEffect} from "react";
-import {
-  Text,
-  View,
-  StyleSheet,
-  SafeAreaView,
-  TextInput,
-  Platform,
-  TouchableOpacity,
-  FlatList,
-}from "react-native";
+import { Text, View, StyleSheet, TextInput}from "react-native";
 
-import { Button } from "../components/Button";
-import { SkillCard } from "../components/SkillCard";
-
-interface SkillData {
+interface PeopleData{
   id: string;
   name: string;
+  phone?: string; //Opicional
 }
 
 export function Home(){
-  const [newSkill, setNewSkill] = useState('');
-  const [mySkill, setMySkill] = useState<SkillData[]>([]);
-  const [greetings, setGreetings] = useState('');
 
-  function handleAddNewSkill(){
-    const data = {
-      id: String(new Date().getTime()),
-      name: newSkill,
+  const [newName, setName] = useState('');
+  const [newPhone, setPhone] = useState('');
+  const [myPeople, setPeople] = useState<PeopleData[]>([]);
+
+  function addPeople(){
+    const data ={
+      id: String(new Date().getTime),
+      name: newName,
+      phone: newPhone
     }
 
-    /*
-    CTRL + M = Abrir o comando de DEBUG
-    Ir no console do navegador
-    console.log("New Skill",data);
-    */
+    setPeople(oldState => [...oldState, data]);
 
-    setMySkill(oldState => [...oldState, data]);
   }
-
-  function handleRemoveSkill(id: string){
-    setMySkill(oldState => oldState.filter(
-      skill => skill.id !== id
-    ))
-  }
-
-  useEffect(() => {
-    const currentHour =  new Date().getHours();
-
-    if(currentHour < 12){
-     setGreetings('Good morning');
-    }else if(currentHour >= 12 && currentHour < 18 ){
-      setGreetings('Good afternoon');
-    }else{
-      setGreetings('Good night');
-    }
-
-  }, [])
 
   return (
     <Fragment>
-        <View style={style.container}>
-          <Text style={style.title}>{greetings}, <Text style={{color:'blue'}}>Igor Barros.</Text></Text>
+      <View style={style.container}>
+        <Text style={style.text}>Nome completo:</Text>
+        <TextInput style={style.textIn} onChangeText={setName}/>
 
-          <TextInput
-            style={style.input}
-            placeholder="New skill"
-            placeholderTextColor="#555"
-            onChangeText={setNewSkill}
-          />
+        <Text style={style.text}>Telefone:</Text>
+        <TextInput style={style.textIn} onChangeText={setPhone}/>
 
-          <Button title="add" onPress={handleAddNewSkill}/>
+        <Text>Nome é {newName} e seu telefone e {newPhone} </Text>
 
-          <Text  style={[style.title, {marginVertical: 50}]}>MySkill</Text>
-
-          <FlatList
-            data={mySkill}
-            //keyExtractor={item => Math.random().toString(36).substr(2, 9)}
-            keyExtractor={item => item.id}
-            renderItem={({item}) => (
-              <SkillCard
-                skill={item.name}
-                onPress={() => handleRemoveSkill(item.id)}
-              />
-          )}
-
-          />
-
-        </View>
+      </View>
     </Fragment>
-  )};
+  )
+};
 
 const style = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor: '#121015',
-    paddingHorizontal: 20,
-    paddingVertical: 20
+  container: {
+    backgroundColor: "#C36A2D",
+    flex: 1,
+    padding: 24
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: 'white',
-    marginBottom : 5
+  text : {
+    color: "#FFF",
+    fontSize: 20,
+    fontWeight: "bold"
   },
-  input: {
-    backgroundColor: '#1F1E25',
-    color: '#FFF',
-    fontSize: 18,
-    padding: Platform.OS === 'ios' ? 15 : 10,
-    marginBottom: 30,
-    borderRadius: 7
-  },
-  rodape: {
-    alignSelf: 'center',
-    marginBottom: 20,
-    color: 'red'
-  },
-  greetings : {
-    color : '#FFF'
-  },
+  textIn : {
+    backgroundColor: "#FFF",
+    borderRadius: 10,
+    padding: 8,
+    color: "#212121",
+    marginBottom: 10
+
+  }
 });
 
